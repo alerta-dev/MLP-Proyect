e9d243c9740cf501d0cda8f84b81b40082852711
AlertDev <49847759+alerta-dev@users.noreply.github.com>
2025-11-16 08:32:46 -0300

    Update cargarVideo.js

--- cargarVideo.js
+++ cargarVideo.js
@@ -1,5 +1,7 @@
 fetch("./videos.json")
   .then(res => res.json())
   .then(data => {
-    document.getElementById("videoPlayer").src = data.video;
-  });
+    const player = document.getElementById("videoPlayer");
+    player.src = data[0].url;
+  })
+  .catch(err => console.error("Error cargando videos.json:", err));
